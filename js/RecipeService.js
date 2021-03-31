60f7a813a3c25596f523756403740383d007c780
Laurent H <laurent.hirth.68@gmail.com>
2021-03-31 17:04:29 +0200

    Tags selecting

--- js/RecipeService.js
+++ js/RecipeService.js
@@ -1,21 +1,69 @@
 import { recipes } from './recipes.js'
 
 export class RecipeService {
+	constructor() {
+		this.allRecipes = recipes
+		this.allIngredient = [...new Set(this.allRecipes.flatMap((recipe) => recipe.ingredients.map((ingredient) => ingredient.ingredient)).sort())]
+		this.allAppliance = [...new Set(this.allRecipes.map((recipe) => recipe.appliance).sort())]
+		this.allUstensils = [...new Set(this.allRecipes.flatMap((recipe) => recipe.ustensils).sort())]
+		this.selectedIngredients = []
+		this.selectedUstensils = []
+		this.selectedAppliance = []
+	}
 
 	getAllRecipes() {
-		return recipes
+		return this.allRecipes
 	}
 
 	getAllIngredients() {
-		return new Set(recipes.map(recipe=>recipe.ingredients.map(ingredient=>ingredient.ingredient)).flat().sort())
+		return this.allIngredient
 	}
 
 	getAllAppliance() {
-		return new Set(recipes.map(recipe=>recipe.appliance).sort())
+		return this.allAppliance
 	}
 
 	getAllUstensils() {
-		return new Set(recipes.map(recipe=>recipe.ustensils).flat().sort())
+		return this.allUstensils
+	}
+
+	addSelectedIngredient(ingredient) {
+		if (this.allIngredient.indexOf(ingredient) != -1) this.selectedIngredients.push(this.allIngredient.splice(this.allIngredient.indexOf(ingredient), 1))
+	}
+	addSelectedUstensil(ustensil) {
+		if (this.allUstensils.indexOf(ustensil) != -1) this.selectedUstensils.push(this.allUstensils.splice(this.allUstensils.indexOf(ustensil), 1))
+	}
+	addSelectedAppliance(appliance) {
+		if (this.allAppliance.indexOf(appliance) != -1) this.selectedAppliance.push(this.allAppliance.splice(this.allAppliance.indexOf(appliance), 1))
+	}
+
+	removeSelectedIngredient(ingredient) {
+		if (this.selectedIngredients.indexOf(ingredient) != -1) {
+			this.allIngredient.push(this.selectedIngredients.splice(this.selectedIngredients.indexOf(ingredient), 1))
+			this.allIngredient.sort()
+		}
+	}
+	removeSelectedUstensil(ustensil) {
+		if (this.selectedUstensils.indexOf(ustensil) != -1) {
+			this.allUstensils.push(this.selectedUstensils.splice(this.selectedUstensils.indexOf(ustensil), 1))
+			this.allUstensils.sort()
+		}
+	}
+	removeSelectedAppliance(appliance) {
+		if (this.selectedAppliance.indexOf(appliance) != -1) {
+			this.allAppliance.push(this.selectedAppliance.splice(this.selectedAppliance.indexOf(appliance), 1))
+			this.allAppliance.sort()
+		}
+	}
+
+	getSelectedIngredients() {
+		return this.selectedIngredients
+	}
+	getSelectedUstensils() {
+		return this.selectedUstensils
+	}
+	getSelectedAppliance() {
+		return this.selectedAppliance
 	}
 
 	findRecipes() {
--- js/index.js
+++ js/index.js
@@ -2,29 +2,111 @@ import { RecipeService } from './RecipeService.js'
 
 const recipeService = new RecipeService()
 
+function displaySelectedTags() {
+	const selectedTagsDiv = document.querySelector('.tags')
+	const selectedIngredients = recipeService.getSelectedIngredients()
+	const selectedUstensils = recipeService.getSelectedUstensils()
+	const selectedAppliance = recipeService.getSelectedAppliance()
+	
+	selectedTagsDiv.innerHTML = ''
+
+	selectedIngredients.forEach((ingredient) => {
+		const buttonIngredient = document.createElement('button')
+		buttonIngredient.classList.add('btn', 'btn-primary', 'btn-sm')
+		buttonIngredient.setAttribute('type', 'button')
+		buttonIngredient.textContent = ingredient + ' ❌'
+		buttonIngredient.addEventListener('click', (e) => {
+			e.preventDefault()
+			recipeService.removeSelectedIngredient(ingredient)
+			displaySelectedTags()
+			displayDropdowns()
+		})
+		selectedTagsDiv.append(buttonIngredient)
+	})
+
+	selectedAppliance.forEach((appliance) => {
+		const buttonAppliance = document.createElement('button')
+		buttonAppliance.classList.add('btn', 'btn-success', 'btn-sm')
+		buttonAppliance.setAttribute('type', 'button')
+		buttonAppliance.textContent = appliance + ' ❌'
+		buttonAppliance.addEventListener('click', (e) => {
+			e.preventDefault()
+			recipeService.removeSelectedAppliance(appliance)
+			displaySelectedTags()
+			displayDropdowns()
+		})
+		selectedTagsDiv.append(buttonAppliance)
+	})
+
+	selectedUstensils.forEach((ustensil) => {
+		const buttonUstensil = document.createElement('button')
+		buttonUstensil.classList.add('btn', 'btn-danger', 'btn-sm')
+		buttonUstensil.setAttribute('type', 'button')
+		buttonUstensil.textContent = ustensil + ' ❌'
+		buttonUstensil.addEventListener('click', (e) => {
+			e.preventDefault()
+			recipeService.removeSelectedUstensil(ustensil)
+			displaySelectedTags()
+			displayDropdowns()
+		})
+		selectedTagsDiv.append(buttonUstensil)
+	})
+
+}
+
 function displayDropdowns() {
 	const dropdownIngredient = document.querySelector('.ingredients')
 	const dropdownAppliance = document.querySelector('.appliance')
 	const dropdownUstensils = document.querySelector('.ustensils')
 
+	dropdownIngredient.innerHTML = ''
+	dropdownAppliance.innerHTML = ''
+	dropdownUstensils.innerHTML = ''
+
 	recipeService.getAllIngredients().forEach((ingredient) => {
-		dropdownIngredient.innerHTML += getDropdownHtmlBloc(ingredient)
+		const htmlBloc = getDropdownHtmlBloc(ingredient)
+		htmlBloc.addEventListener('click', () => {
+			recipeService.addSelectedIngredient(ingredient)
+			displaySelectedTags()
+			displayDropdowns()
+		})
+		dropdownIngredient.append(htmlBloc)
 	})
 
 	recipeService.getAllAppliance().forEach((appliance) => {
-		dropdownAppliance.innerHTML += getDropdownHtmlBloc(appliance)
+		const htmlBloc = getDropdownHtmlBloc(appliance)
+		htmlBloc.addEventListener('click', () => {
+			recipeService.addSelectedAppliance(appliance)
+			displaySelectedTags()
+			displayDropdowns()
+		})
+		dropdownAppliance.append(htmlBloc)
 	})
 
 	recipeService.getAllUstensils().forEach((ustensil) => {
-		dropdownUstensils.innerHTML += getDropdownHtmlBloc(ustensil)
+		const htmlBloc = getDropdownHtmlBloc(ustensil)
+		htmlBloc.addEventListener('click', () => {
+			recipeService.addSelectedUstensil(ustensil)
+			displaySelectedTags()
+			displayDropdowns()
+		})
+		dropdownUstensils.append(htmlBloc)
 	})
 
 	function getDropdownHtmlBloc(text) {
-		return `<div class="col-4">
-			<a class="dropdown-item" href="#">
-				${text}
-			</a>
-		</div>`
+		const a = document.createElement('a')
+		const div = document.createElement('div')
+
+		div.classList.add('col-4')
+		a.classList.add('dropdown-item')
+		a.textContent = text
+
+		a.addEventListener('click', (e) => {
+			e.preventDefault()
+		})
+
+		div.append(a)
+		return div
 	}
 }
 
@@ -55,5 +137,6 @@ function displayRecipes() {
 	})
 }
 
+displaySelectedTags()
 displayDropdowns()
 displayRecipes()
